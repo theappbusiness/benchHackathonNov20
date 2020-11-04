3f76d59ee2411056d60190ffe813bfbe49481cec
Gareth Miller <gmiller290488@gmail.com>
2020-11-04 11:44:33 +0000

    Initial project setup

---
+++ shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.kcc.kmmhackathon.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.kcc.kmmhackathon.shared
+
+expect class Platform() {
+    val platform: String
+}
