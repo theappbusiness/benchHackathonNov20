9c66616d90bf2cb909629890f697bac3152fa1e3
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-11-09 15:10:08 +0000

    Moved meal creating inside the button closure

--- iosHackthonApp/iosHackthonApp/Add Meal Screen/AddMealView.swift
+++ iosHackthonApp/iosHackthonApp/Add Meal Screen/AddMealView.swift
@@ -16,16 +16,16 @@ struct AddMealView: View {
 	var body: some View {
 		//TODO: Pass the actual values when UI is complete
 		let sdk = MealApi()
-		let meal = Meal(id: "\(sdk.getUUID())",
-										name: "Pizza",
-										quantity: 2,
-										availableFromDate: "Tuesday",
-										expiryDate: "Saturday",
-										info: "Meat",
-										hot: false,
-										locationLat: 51.509865,
-										locationLong:  -0.118092)
 		Button("Add a meal") {
+			let meal = Meal(id: "\(sdk.getUUID())",
+											name: "Pizza",
+											quantity: 2,
+											availableFromDate: "Tuesday",
+											expiryDate: "Saturday",
+											info: "Meat",
+											hot: false,
+											locationLat: 51.509865,
+											locationLong:  -0.118092)
 			self.viewModel.postMeal(meal: meal)
 		}
 	}
