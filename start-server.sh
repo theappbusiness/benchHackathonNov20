a307f5ee1e5b1945a6914eb33627df67c1498934
Dan Smtih <dan.smith@kinandcarta.com>
2020-11-09 10:56:11 +0000

    Updates start-server script

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-cd shared/src/ && json-server --watch db.json
+
+if nc 127.0.0.1 3000 < /dev/null
+then
+	echo "Server already running on localhost:3000"
+else
+	echo "Launching meals mock server"
+  cd shared/src/ && json-server --watch db.json
+fi
