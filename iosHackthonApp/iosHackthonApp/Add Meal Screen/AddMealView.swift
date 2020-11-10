943dc310dafdd0243f800460706ebee20fbb5230
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-11-10 11:59:38 +0000

    Changes

--- iosHackthonApp/iosHackthonApp/Add Meal Screen/AddMealView.swift
+++ iosHackthonApp/iosHackthonApp/Add Meal Screen/AddMealView.swift
@@ -8,49 +8,84 @@
 
 import SwiftUI
 import shared
+import CoreLocation
 
 struct AddMealView: View {
 
 	@ObservedObject private(set) var viewModel: ViewModel
 	@State var title: String = ""
+	@State var additionalInfo: String = ""
 	@State var quantity: String = ""
 	@State var hot: Bool = false
 	@State var availableFromDate = Date()
 	@State var useByDate = Date()
+	@State var address: String = ""
 
 	var body: some View {
 
-
 		ScrollView {
 			VStack(alignment: .leading, spacing: 10) {
-				Text("Title")
-				TextField("", text: $title)
-					.padding(.all)
-					.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
 
+				Group {
+					Text("Title")
+					TextField("", text: $title)
+						.padding(.all)
+						.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
 
-				Text("Quantity")
-				TextField("", text: $quantity)
-					.keyboardType(.numberPad)
-					.padding(.all)
-					.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+					Text("Additional Information")
+					TextField("", text: $additionalInfo)
+						.keyboardType(.numberPad)
+						.padding(.all)
+						.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
 
-				Text("Temperature")
-				HStack {
-					Button("Hot", action: {
-						hot = true
-					})
-					Button("Cold", action: {
-						hot = false
-					})
+					Text("Quantity")
+					TextField("", text: $quantity)
+						.keyboardType(.numberPad)
+						.padding(.all)
+						.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+
+					//	Spacer()
+
+					Text("Temperature")
+					HStack {
+						Button("Hot", action: {
+							hot = true
+						})
+						Button("Cold", action: {
+							hot = false
+						})
+					}
 				}
 
 
-				DatePicker("Available From", selection: $availableFromDate, in: Date()..., displayedComponents: .date)
+				Spacer()
+				Group {
+					DatePicker("Available From", selection: $availableFromDate, in: Date()..., displayedComponents: .date)
 
-				DatePicker("Use By", selection: $useByDate, in: Date()..., displayedComponents: .date)
+					DatePicker("Use By", selection: $useByDate, in: Date()..., displayedComponents: .date)
+
+				}
 
 
+				Spacer()
+				Group {
+					Text("Address")
+					HStack {
+						TextField("", text: $address)
+							.keyboardType(.numberPad)
+							.padding(.all)
+							.background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+						Spacer()
+						Spacer()
+						Button(action: {
+							print("location button tapped!")
+						}) {
+							Image(systemName: "location")
+								.font(.title)
+								.foregroundColor(.green)
+						}
+					}
+				}
 
 				Spacer()
 				//TODO: Pass the actual values when UI is complete
@@ -76,37 +111,28 @@ struct AddMealView: View {
 			}.padding()
 
 		}
-
-
-
-
-
-
-
-
-
 	}
 }
 
 extension AddMealView {
-    class ViewModel: ObservableObject {
+	class ViewModel: ObservableObject {
 
-        let sdk: MealsSDK
+		let sdk: MealsSDK
 
-        init(sdk: MealsSDK) {
-            self.sdk = sdk
-        }
+		init(sdk: MealsSDK) {
+			self.sdk = sdk
+		}
 
-        func postMeal(meal: Meal) {
-            sdk.postMeal(meal: meal, completionHandler: { response, test in
-                print("Response \(response)")
-            })
-        }
-    }
+		func postMeal(meal: Meal) {
+			sdk.postMeal(meal: meal, completionHandler: { response, test in
+				print("Response \(response)")
+			})
+		}
+	}
 }
 
 struct AddMealView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddMealView(viewModel: AddMealView.ViewModel(sdk: MealsSDK()))
-    }
+	static var previews: some View {
+		AddMealView(viewModel: AddMealView.ViewModel(sdk: MealsSDK()))
+	}
 }
