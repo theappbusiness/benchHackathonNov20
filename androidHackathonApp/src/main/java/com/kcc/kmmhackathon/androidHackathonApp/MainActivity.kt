1b0e7e565ef94c8aad942efe3e06c6303f7263e2
Roger Tan <roger.tan@kinandcarta.com>
2020-11-10 12:02:31 +0000

    WIP first screen android

--- androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/MainActivity.kt
+++ androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.kcc.kmmhackathon.androidHackathonApp
 
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kcc.kmmhackathon.shared.Greeting
 import android.widget.TextView
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle(R.string.heading)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val listMealButton = findViewById<ActionButton>(R.id.list_meal)
+        listMealButton.setIcon(R.drawable.ic_baseline_search_24)
+        listMealButton.setText(R.string.findButton)
+
+        val addMealButton = findViewById<ActionButton>(R.id.add_meal)
+        addMealButton.setIcon(R.drawable.ic_outline_add_box_24)
+        addMealButton.setText(R.string.plusButton)
+
     }
 }
