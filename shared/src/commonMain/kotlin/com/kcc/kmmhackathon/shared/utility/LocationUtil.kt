b539094e19b90ff5989133e9ca3982ff8edfba09
Dan Smtih <dan.smith@kinandcarta.com>
2020-11-16 12:59:49 +0000

    Adds locationUtil and getDistance function to KMM

---
+++ shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/utility/LocationUtil.kt
@@ -0,0 +1,29 @@
+package com.kcc.kmmhackathon.shared.utility
+
+import kotlin.math.*
+
+typealias Radian = Double
+typealias Degree = Double
+
+class LocationUtil {
+
+    fun getDistance(lat1: Double, lon1: Double,lat2: Double, lon2: Double, inMiles: Boolean): Double {
+
+        // Haversine formula
+        val earthRadius = 3958.75
+        val dLat = (lat1-lat2).toRadian()
+        val dLon = (lon1-lon2).toRadian()
+
+        val a: Double = sin(dLat / 2) * sin(dLat / 2) +
+                cos(lat2.toRadian()) * cos(lat1.toRadian()) *
+                sin(dLon / 2) * sin(dLon / 2);
+        val c: Double = 2 * atan2(sqrt(a), sqrt(1 - a))
+        val distance: Double = earthRadius * c
+
+        return if (inMiles) { distance.round(2) } else { distance.toKilometres().round(2) }
+    }
+
+    // Helper functions
+    private fun Degree.toRadian(): Radian = this / 180 * PI
+}
+
