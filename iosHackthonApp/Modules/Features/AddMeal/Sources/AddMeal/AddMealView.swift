e8a9478a81e3f981ef43f8f071b245ddaf75323d
Gareth Miller <gmiller290488@gmail.com>
2020-11-18 11:44:37 +0000

    Changed the functions creating views to be structs

--- iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealView.swift
+++ iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealView.swift
@@ -16,38 +16,56 @@ import Components
 public struct AddMealView: View {
 
     public init(viewModel: AddMealViewModel) {
-        _viewModel = StateObject(wrappedValue: viewModel)
+        self.viewModel = viewModel
     }
 
-    @StateObject private var viewModel: AddMealViewModel
+    @ObservedObject var viewModel: AddMealViewModel
+
+    @State var title: String = ""
+    @State var additionalInfo: String = ""
+    @State var quantity: Int = 0
+    @State var availableFromDate = Date()
+    @State var useByDate = Date()
+    @State var address: String = ""
+    @State var latitude: Float = 0
+    @State var longitude: Float = 0
+    @State var isHot: Bool = true
+
+    var buttonIsDisabled: Bool {
+        title.isEmpty || additionalInfo.isEmpty || address.isEmpty || quantity == 0
+    }
+
+    var buttonColor: Color {
+        buttonIsDisabled ? ColorManager.gray : ColorManager.appPrimary
+    }
 
     public var body: some View {
         ZStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 10) {
-                    createTextFields()
-                    createTemperatureButtons()
+                    infoTextFieldsGroup(title: $title, info: $additionalInfo, quantity: $quantity)
+                    TemperatureButtonsGroup(isHot: $isHot)
                     Spacer()
-                    createDatePickers()
+                    DatePickers(availableFromDate: $availableFromDate, useByDate: $useByDate)
                     Spacer()
-                    createAddressFields()
+                    AddressTextField(locationManager: viewModel.locationManager, address: $address, latitude: $latitude, longitude: $longitude)
                     Spacer()
                     GeometryReader { geometry in
                         Button(action: {
-                            self.viewModel.postMeal()
+                            self.viewModel.postMeal(meal: createMeal())
                         }) {
                             Text(Strings.AddMealScreen.addMeal)
                                 .modifier(AddButtonStyle(width: geometry.size.width,
-                                                         backgroundColor: viewModel.buttonColor))
+                                                         backgroundColor: buttonColor))
                         }
-                        .disabled(viewModel.buttonIsDisabled)
+                        .disabled(buttonIsDisabled)
                     }
                     Spacer()
                 }.padding()
             }
             .navigationBarTitle(Strings.AddMealScreen.addMeal)
             .onAppear() {
-                viewModel.address = "\(self.viewModel.locationManager.address)"
+                address = "\(viewModel.locationManager.address)"
             }
         }
         .alert(isPresented: $viewModel.showingAlert) {
@@ -65,72 +83,125 @@ public struct AddMealView: View {
             }
         }
     }
+}
 
-    func createTextFields() -> some View {
-        Group {
-            Text(Strings.AddMealScreen.title)
-            TextField(Strings.AddMealScreen.titlePlaceholder, text: $viewModel.title)
-                .modifier(GreyTextFieldStyle())
+// MARK:- Views
+extension AddMealView {
 
-            Text(Strings.AddMealScreen.additionalInfo)
-            TextField(Strings.AddMealScreen.additionalInfoPlaceholder, text: $viewModel.additionalInfo)
-                .modifier(GreyTextFieldStyle())
+    private struct infoTextFieldsGroup: View {
 
-            Text(Strings.AddMealScreen.quantity)
-            TextField(Strings.AddMealScreen.quantity, value: $viewModel.quantity, formatter: NumberFormatter())
-                .modifier(GreyTextFieldStyle())
-        }
-    }
+        @Binding var title: String
+        @Binding var info: String
+        @Binding var quantity: Int
 
-    func createTemperatureButtons() -> some View {
-        Group {
-            Text(Strings.AddMealScreen.temperature)
-            HStack {
-                Button(action: {
-                    viewModel.isHot.toggle()
-                }) {
-                    Image(systemName: Strings.Common.Images.hotFood)
-                        .modifier(IconButtonImageStyle(color: viewModel.hotFoodColor))
-                }
-
-                Button(action: {
-                    viewModel.isHot.toggle()
-                }) {
-                    Image(systemName: Strings.Common.Images.coldFood)
-                        .modifier(IconButtonImageStyle(color: viewModel.coldFoodColor))
-                }
-            }
-        }
-    }
-
-    func createDatePickers() -> some View {
-        Group {
-            // TODO: Not allow useBy date to be before available from
-            DatePicker(Strings.AddMealScreen.availableFrom, selection: $viewModel.availableFromDate, in: Date()..., displayedComponents: .date)
-            DatePicker(Strings.AddMealScreen.useBy, selection: $viewModel.useByDate, in: Date()..., displayedComponents: .date)
-        }
-    }
-
-    func createAddressFields() -> some View {
-        Group {
-            Text(Strings.AddMealScreen.address)
-            HStack {
-                TextField("", text: $viewModel.address)
+        var body: some View {
+            Group {
+                Text(Strings.AddMealScreen.title)
+                TextField(Strings.AddMealScreen.titlePlaceholder, text: $title)
                     .modifier(GreyTextFieldStyle())
-                    .disabled(true)
 
-                Spacer(minLength: 10)
-                Button(action: {
-                    viewModel.address = "\(viewModel.locationManager.address)"
-                    viewModel.latitude = Float(viewModel.locationManager.userLatitude)
-                    viewModel.longitude = Float(viewModel.locationManager.userLongitude)
-                }) {
-                    Image(systemName: Strings.Common.Images.location)
-                        .font(.title)
-                        .foregroundColor(ColorManager.appPrimary)
+                Text(Strings.AddMealScreen.additionalInfo)
+                TextField(Strings.AddMealScreen.additionalInfoPlaceholder, text: $info)
+                    .modifier(GreyTextFieldStyle())
+
+                Text(Strings.AddMealScreen.quantity)
+                TextField(Strings.AddMealScreen.quantity, value: $quantity, formatter: NumberFormatter())
+                    .modifier(GreyTextFieldStyle())
+            }
+        }
+    }
+
+    private struct TemperatureButtonsGroup: View {
+
+        @Binding var isHot: Bool
+
+        var hotFoodColor: Color {
+            isHot ? ColorManager.red : ColorManager.gray
+        }
+
+        var coldFoodColor: Color {
+            isHot ? ColorManager.gray : ColorManager.blue
+        }
+
+        var body: some View {
+            Group {
+                Text(Strings.AddMealScreen.temperature)
+                HStack {
+                    Button(action: {
+                    isHot.toggle()
+                    }) {
+                        Image(systemName: Strings.Common.Images.hotFood)
+                            .modifier(IconButtonImageStyle(color: hotFoodColor))
+                    }
+
+                    Button(action: {
+                        isHot.toggle()
+                    }) {
+                        Image(systemName: Strings.Common.Images.coldFood)
+                            .modifier(IconButtonImageStyle(color: coldFoodColor))
+                    }
                 }
             }
         }
+    }
+
+    private struct DatePickers: View {
+
+        @Binding var availableFromDate: Date
+        @Binding var useByDate: Date
+
+        var body: some View {
+            Group {
+                DatePicker(Strings.AddMealScreen.availableFrom, selection: $availableFromDate, in: Date()..., displayedComponents: .date)
+                DatePicker(Strings.AddMealScreen.useBy, selection: $useByDate, in: availableFromDate..., displayedComponents: .date)
+            }
+        }
+    }
+
+    private struct AddressTextField: View {
+
+        let locationManager: LocationManager
+        @Binding var address: String
+        @Binding var latitude: Float
+        @Binding var longitude: Float
+
+        var body: some View {
+            Group {
+                Text(Strings.AddMealScreen.address)
+                HStack {
+                    TextField("", text: $address)
+                        .modifier(GreyTextFieldStyle())
+                        .disabled(true)
+
+                    Spacer(minLength: 10)
+                    Button(action: {
+                        address = "\(locationManager.address)"
+                        latitude = Float(locationManager.userLatitude)
+                        longitude = Float(locationManager.userLongitude)
+                    }) {
+                        Image(systemName: Strings.Common.Images.location)
+                            .font(.title)
+                            .foregroundColor(ColorManager.appPrimary)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//MARK:- Functions
+extension AddMealView {
+    private func createMeal() -> Meal {
+
+        Meal(id: "\(viewModel.sdk.getUUID())",
+             name: "\(title)",
+             quantity: Int32(quantity),
+             availableFromDate: "\(availableFromDate)",
+             expiryDate: "\(useByDate)",
+             info: "\(additionalInfo)",
+             hot: isHot,
+             locationLat: latitude,
+             locationLong:  longitude)
     }
 }
 
--- iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealViewModel.swift
+++ iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealViewModel.swift
@@ -26,51 +26,13 @@ public final class AddMealViewModel: ObservableObject {
     @Published var code = ""
     @Published var showingAlert = false
     @Published var activeAlert: ActiveAlert = .collection
-    @Published var title: String = ""
-    @Published var additionalInfo: String = ""
-    @Published var quantity: Int = 0
-    @Published var availableFromDate = Date()
-    @Published var useByDate = Date()
-    @Published var address: String = ""
-    @Published var latitude: Float = 0
-    @Published var longitude: Float = 0
-    @Published var isHot: Bool = true
-
-    var buttonIsDisabled: Bool {
-        title.isEmpty || additionalInfo.isEmpty || address.isEmpty || quantity == 0
-    }
-
-    var hotFoodColor: Color {
-        isHot ? ColorManager.red : ColorManager.gray
-    }
-
-    var coldFoodColor: Color {
-        isHot ? ColorManager.gray : ColorManager.blue
-    }
-
-    var buttonColor: Color {
-        buttonIsDisabled ? ColorManager.gray : ColorManager.appPrimary
-    }
 }
 
 extension AddMealViewModel {
 
-    func createMeal() -> Meal {
-
-        Meal(id: "\(sdk.getUUID())",
-            name: "\(title)",
-            quantity: Int32(quantity),
-            availableFromDate: "\(availableFromDate)",
-            expiryDate: "\(useByDate)",
-            info: "\(additionalInfo)",
-            hot: isHot,
-            locationLat: latitude,
-            locationLong:  longitude)
-    }
-
-    func postMeal() {
+    func postMeal(meal: Meal) {
         
-        sdk.postMeal(meal: createMeal(), completionHandler: { meal, error in
+        sdk.postMeal(meal: meal, completionHandler: { meal, error in
             guard
                 let meal = meal,
                 error == nil else {
