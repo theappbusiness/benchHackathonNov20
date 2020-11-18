b74a042d1d3ca525d0e10130bc526faa19257973
Kirsty Butler <kirstybutler6@gmail.com>
2020-11-18 16:50:45 +0000

    Creating bottom nav view

--- androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/MainActivity.kt
+++ androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/MainActivity.kt
@@ -3,28 +3,24 @@ package com.kcc.kmmhackathon.androidHackathonApp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kinandcarta.lib.find.meal.view.FindMealActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setTitle(R.string.app_name)
 
-        val addMealButton = findViewById<ActionButton>(R.id.add_meal_button)
-        addMealButton.setIcon(R.drawable.ic_outline_add_box_24)
-        addMealButton.setText(R.string.add_meal_title)
-        addMealButton.setOnClickListener {
-            val intent = Intent(this, AddMealActivity::class.java)
-            startActivity(intent)
-        }
+        bottomNavigationView = findViewById(R.id.bottom_nav)
 
-        val findMealButton = findViewById<ActionButton>(R.id.find_meal_button)
-        findMealButton.setIcon(R.drawable.ic_baseline_search_24)
-        findMealButton.setText(R.string.find_meal_title)
-        findMealButton.setOnClickListener {
-            val intent = Intent(this, FindMealActivity::class.java)
-            startActivity(intent)
-        }
+        val intent = Intent(this, AddMealActivity::class.java)
+        startActivity(intent)
+
+        val intent2 = Intent(this, FindMealActivity::class.java)
+        startActivity(intent)
     }
 }
