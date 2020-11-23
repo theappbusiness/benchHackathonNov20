6c1e6f85dbfab209345ede414447a0eee10a204e
Leke Abolade <adeleke.abolade@theappbusiness.com>
2020-11-23 09:24:34 +0000

    Adding authentication objects

--- shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/entity/Entity.kt
+++ shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/entity/Entity.kt
@@ -39,3 +39,15 @@ data class MealWithDistance(
     @SerialName("distance")
     val distance: Double
 )
+
+@Serializable
+data class FirebaseAuthenticationResponse(
+    val kind: String? = null,
+    val idToken: String? = null,
+    val email: String? = null,
+    val refreshToken: String? = null,
+    val expiresIn: String? = null,
+    val localId: String? = null,
+    val code: Int? = null,
+    val message: String? = null
+)
---
+++ shared/src/commonMain/kotlin/com/kcc/kmmhackathon/shared/network/FirebaseAuthenticationApi.kt
@@ -0,0 +1,62 @@
+import com.kcc.kmmhackathon.shared.entity.FirebaseAuthenticationResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import kotlinx.serialization.json.Json
+import io.ktor.http.*
+
+class FirebaseAuthenticationStore : AuthenticationStore {
+
+ private val httpClient = HttpClient {
+        install(JsonFeature) {
+            val json = Json { ignoreUnknownKeys = true }
+            serializer = KotlinxSerializer(json)
+        }
+    }
+
+    override suspend fun signUp(
+        apiKey: String,
+        email: String,
+        password: String,
+        returnSecureToken: Boolean
+    ) = handleAuthenticationRequest(apiKey,
+        ENDPOINT_SIGN_UP, email, password, returnSecureToken)
+
+    override suspend fun signIn(
+        apiKey: String,
+        email: String,
+        password: String,
+        returnSecureToken: Boolean
+    ) = handleAuthenticationRequest(apiKey,
+        ENDPOINT_SIGN_IN, email, password, returnSecureToken)
+
+    private suspend fun handleAuthenticationRequest(
+        apiKey: String,
+        endpoint: String,
+        email: String,
+        password: String,
+        returnSecureToken: Boolean
+    ): FirebaseAuthenticationResponse {
+        return try {
+            httpClient.post {
+                url("$BASE_URL$endpoint")
+                parameter("key", apiKey)
+                parameter("email", email)
+                parameter("password", password)
+                parameter("returnSecureToken", returnSecureToken)
+            }
+        } catch (cause: Throwable) {
+            FirebaseAuthenticationResponse(
+                message = cause.message ?: ""
+            )
+        }
+    }
+
+    companion object {
+        private const val ENDPOINT_SIGN_UP = "signUp"
+        private const val ENDPOINT_SIGN_IN = "signInWithPassword"
+        private const val BASE_URL = "https://identitytoolkit.googleapis.com/v1/accounts:"
+    }
+}
