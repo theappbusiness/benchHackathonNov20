7efd2dc0e4264d6a95bba113c4255aa4773ebb8d
Gareth Miller <gmiller290488@gmail.com>
2020-11-24 15:48:06 +0000

    Move repeated tab bar code to a component

--- iosHackthonApp/Modules/Features/TabBar/Sources/TabBar/TabAppView.swift
+++ iosHackthonApp/Modules/Features/TabBar/Sources/TabBar/TabAppView.swift
@@ -13,57 +13,46 @@ import Theming
 import MealList
 import Strings
 import Settings
+import Components
 
 public struct TabAppView: View {
-	
-	private let sdk = MealsSDK()
-	private let locationManager = LocationManager()
-	
-	@State var selectedView: Int
-	
-	public init(selectedView: Int) {
-		UITabBar.appearance().barTintColor = UIColor(ColorManager.appPrimary)
-		_selectedView = State(initialValue: selectedView)
-	}
-	
-	public var body: some View {
-		TabView(selection: $selectedView, content: {
-			NavigationView {
-				MealMapView(viewModel: .init(sdk: sdk, locationManager: locationManager))
-			}
-			.tabItem {
-				Image(systemName: Strings.TabView.Images.find)
-				Text(Strings.TabView.findButtonText)
-			}.tag(0)
-			.accentColor(ColorManager.appPrimary)
-            .navigationBarHidden(true)
-			
-			NavigationView {
-				MealListView(viewModel: .init(sdk: sdk, locationManager: locationManager))
-			}
-			.tabItem {
-				Image(systemName: Strings.TabView.Images.listView)
-				Text(Strings.TabView.listViewText)
-			}.tag(1)
-			.accentColor(ColorManager.appPrimary)
-            .navigationBarHidden(true)
-			
-			NavigationView {
-                SettingsView()
-			}
-			.tabItem {
-				Image(systemName: Strings.TabView.Images.settings)
-				Text(Strings.TabView.settings)
-			}.tag(2)
-            .navigationBarHidden(true)
-			.accentColor(ColorManager.appPrimary)
-		})
-		.accentColor(.white)
-	}
+
+    private let sdk = MealsSDK()
+    private let locationManager = LocationManager()
+
+    @State var selectedView: Int
+
+    public init(selectedView: Int) {
+        UITabBar.appearance().barTintColor = UIColor(ColorManager.appPrimary)
+        _selectedView = State(initialValue: selectedView)
+    }
+
+    public var body: some View {
+        TabView(selection: $selectedView, content: {
+            TabNavigationItem(
+                destination: AnyView(MealMapView(viewModel: .init(sdk: sdk, locationManager: locationManager))),
+                image: Strings.TabView.Images.find,
+                title: Strings.TabView.findButtonText,
+                tagNumber: 0)
+
+            TabNavigationItem(
+                destination: AnyView(MealListView(viewModel: .init(sdk: sdk, locationManager: locationManager))),
+                image: Strings.TabView.Images.listView,
+                title: Strings.TabView.listViewText,
+                tagNumber: 1)
+            
+            TabNavigationItem(
+                destination: AnyView(SettingsView()),
+                image: Strings.TabView.Images.settings,
+                title: Strings.TabView.settings,
+                tagNumber: 2)
+        })
+        .accentColor(.white)
+    }
 }
 
 struct TabAppView_Previews: PreviewProvider {
-	static var previews: some View {
-		TabAppView(selectedView: 1)
-	}
+    static var previews: some View {
+        TabAppView(selectedView: 1)
+    }
 }
