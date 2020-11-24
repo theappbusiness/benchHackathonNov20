f0c18d0fd98bf991dc4cba8cbae96d115b618273
Gareth Miller <gmiller290488@gmail.com>
2020-11-24 09:46:03 +0000

    Renamed testing file and test name

--- shared/src/commonTest/kotlin/com.kcc.kmmhackathon.shared.utility/DateFormattingUtilTest.kt
+++ shared/src/commonTest/kotlin/com.kcc.kmmhackathon.shared.utility/DateFormattingUtilTest.kt
@@ -3,10 +3,11 @@ package com.kcc.kmmhackathon.shared.utility
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class DateFormattingTest {
+class DateFormattingUtilTest {
 
     @Test
-    fun testDateFormattingTest() {
+    fun testDateFormattingUtil_returnsCorrectString() {
         assertEquals(DateFormattingUtil().convertTimeStamp(0), "Thu 1 Jan")
+        assertEquals(DateFormattingUtil().convertTimeStamp(946684800), "Sat 1 Jan") 
     }
 }
