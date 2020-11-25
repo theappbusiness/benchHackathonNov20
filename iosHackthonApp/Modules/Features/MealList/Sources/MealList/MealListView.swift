a62dfd1f3785dcbe7a580a52a59427e703221d29
Gareth Miller <gmiller290488@gmail.com>
2020-11-25 09:29:40 +0000

    Fixing indentation. Removing reference to feature in wrong place

--- iosHackthonApp/Modules/Features/MealList/Sources/MealList/MealListView.swift
+++ iosHackthonApp/Modules/Features/MealList/Sources/MealList/MealListView.swift
@@ -5,28 +5,28 @@ import Strings
 import AddMeal
 
 public struct MealListView: View {
-	
-	@ObservedObject private var viewModel: MealListViewModel
-	
-	public init(viewModel: MealListViewModel) {
-		self.viewModel = viewModel
-	}
-	
-	public var body: some View {
-		VStack {
-			if viewModel.meals.isEmpty {
-				Text("No meals found") // TODO: Do something nicer than this
-			}
-			
-			ScrollView {
-				LazyVStack {
-					ForEach((0 ..< viewModel.meals.count), id: \.self) {
-						MealRow(listViewModel: viewModel, rowViewModel: MealRowViewModel(meal: viewModel.meals[$0]))
-							.padding()
-					}
-				}
-			}
-			.navigationBarTitle(Strings.MealListScreen.title)
+
+    @ObservedObject private var viewModel: MealListViewModel
+
+    public init(viewModel: MealListViewModel) {
+        self.viewModel = viewModel
+    }
+
+    public var body: some View {
+        VStack {
+            if viewModel.meals.isEmpty {
+                Text("No meals found") // TODO: Do something nicer than this
+            }
+
+            ScrollView {
+                LazyVStack {
+                    ForEach((0 ..< viewModel.meals.count), id: \.self) {
+                        MealRow(listViewModel: viewModel, rowViewModel: MealRowViewModel(meal: viewModel.meals[$0]))
+                            .padding()
+                    }
+                }
+            }
+            .navigationBarTitle(Strings.MealListScreen.title)
             .navigationBarItems(leading:
                                     Button(action: {
                                         viewModel.isAddMealShowing = true
@@ -39,34 +39,34 @@ public struct MealListView: View {
                                     }, label: {
                                         Image(systemName: Strings.MealListScreen.Images.reload)
                                     }))
-                .foregroundColor(.white)
+            .foregroundColor(.white)
             .sheet(isPresented: $viewModel.isAddMealShowing, content: {
                 AddMealView(sdk: viewModel.sdk, locationManager: viewModel.locationManager)
             })
-			.alert(isPresented: $viewModel.showingAlert) {
-				switch viewModel.activeAlert {
-				case .unavailable:
-					return  Alert(
-						title: Text(Strings.Common.sorry),
-						message: Text(Strings.MealListScreen.UnavailableAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				case .collection:
-					return Alert(
-						title: Text(viewModel.code),
-						message: Text(Strings.MealListScreen.CollectionAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				case .error:
-					return Alert(
-						title: Text(Strings.Common.sorry),
-						message: Text(Strings.Common.ErrorAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				}
-			}
-		}
-		.onAppear(perform: {
-			viewModel.loadMeals()
-		})
-	}
+            .alert(isPresented: $viewModel.showingAlert) {
+                switch viewModel.activeAlert {
+                case .unavailable:
+                    return  Alert(
+                        title: Text(Strings.Common.sorry),
+                        message: Text(Strings.MealListScreen.UnavailableAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                case .collection:
+                    return Alert(
+                        title: Text(viewModel.code),
+                        message: Text(Strings.MealListScreen.CollectionAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                case .error:
+                    return Alert(
+                        title: Text(Strings.Common.sorry),
+                        message: Text(Strings.Common.ErrorAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                }
+            }
+        }
+        .onAppear(perform: {
+            viewModel.loadMeals()
+        })
+    }
 }
 
 extension Meal: Identifiable { }
--- iosHackthonApp/Modules/Features/Settings/Tests/SettingsTests/SettingsTests.swift
+++ iosHackthonApp/Modules/Features/Settings/Tests/SettingsTests/SettingsTests.swift
@@ -3,13 +3,6 @@ import XCTest
 
 final class SettingsTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Settings().text, "Hello, World!")
+        XCTAssertTrue(true)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
