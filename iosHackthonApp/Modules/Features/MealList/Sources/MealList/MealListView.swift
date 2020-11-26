5feb7baa0fd779b31909e5b812904434abf211ce
Gareth Miller <gmiller290488@gmail.com>
2020-11-26 14:59:15 +0000

    indentation

--- iosHackthonApp/Modules/Features/MealList/Sources/MealList/MealListView.swift
+++ iosHackthonApp/Modules/Features/MealList/Sources/MealList/MealListView.swift
@@ -5,57 +5,57 @@ import Strings
 import Theming
 
 public struct MealListView: View {
-	
-	@ObservedObject private var viewModel: MealListViewModel
-	
+    
+    @ObservedObject private var viewModel: MealListViewModel
+    
     public init(viewModel: MealListViewModel) {
-		self.viewModel = viewModel
-	}
-	
-	public var body: some View {
-		VStack {
-			if viewModel.meals.isEmpty {
-				Text("No meals found") // TODO: Do something nicer than this
-			}
-			ScrollView {
-				LazyVStack {
-					ForEach((0 ..< viewModel.meals.count), id: \.self) {
-						MealRow(listViewModel: viewModel, rowViewModel: MealRowViewModel(meal: viewModel.meals[$0]))
-							.padding()
-					}
-				}
+        self.viewModel = viewModel
+    }
+    
+    public var body: some View {
+        VStack {
+            if viewModel.meals.isEmpty {
+                Text("No meals found") // TODO: Do something nicer than this
             }
-			.navigationBarTitle(Strings.MealListScreen.title)
-			.navigationBarItems(trailing:
-									Button(action: {
-										self.viewModel.loadMeals()
-									}, label: {
-										Image(systemName: Strings.MealListScreen.Images.reload)
-											.foregroundColor(.white)
-									}))
-			.alert(isPresented: $viewModel.showingAlert) {
-				switch viewModel.activeAlert {
-				case .unavailable:
-					return  Alert(
-						title: Text(Strings.Common.sorry),
-						message: Text(Strings.MealListScreen.UnavailableAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				case .collection:
-					return Alert(
-						title: Text(viewModel.code),
-						message: Text(Strings.MealListScreen.CollectionAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				case .error:
-					return Alert(
-						title: Text(Strings.Common.sorry),
-						message: Text(Strings.Common.ErrorAlert.message),
-						dismissButton: .default(Text(Strings.Common.ok)))
-				}
-			}
+            ScrollView {
+                LazyVStack {
+                    ForEach((0 ..< viewModel.meals.count), id: \.self) {
+                        MealRow(listViewModel: viewModel, rowViewModel: MealRowViewModel(meal: viewModel.meals[$0]))
+                            .padding()
+                    }
+                }
+            }
+            .navigationBarTitle(Strings.MealListScreen.title)
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.viewModel.loadMeals()
+                                    }, label: {
+                                        Image(systemName: Strings.MealListScreen.Images.reload)
+                                            .foregroundColor(.white)
+                                    }))
+            .alert(isPresented: $viewModel.showingAlert) {
+                switch viewModel.activeAlert {
+                case .unavailable:
+                    return  Alert(
+                        title: Text(Strings.Common.sorry),
+                        message: Text(Strings.MealListScreen.UnavailableAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                case .collection:
+                    return Alert(
+                        title: Text(viewModel.code),
+                        message: Text(Strings.MealListScreen.CollectionAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                case .error:
+                    return Alert(
+                        title: Text(Strings.Common.sorry),
+                        message: Text(Strings.Common.ErrorAlert.message),
+                        dismissButton: .default(Text(Strings.Common.ok)))
+                }
+            }
         }
-		.onAppear(perform: {
-			viewModel.loadMeals()
-		})
+        .onAppear(perform: {
+            viewModel.loadMeals()
+        })
     }
 }
 
--- iosHackthonApp/Modules/Features/TabBar/Sources/TabBar/TabAppView.swift
+++ iosHackthonApp/Modules/Features/TabBar/Sources/TabBar/TabAppView.swift
@@ -15,46 +15,46 @@ import Strings
 import AddMeal
 
 public struct TabAppView: View {
-	
-	private let sdk = MealsSDK()
-	private let locationManager = LocationManager()
-	
-	@State var selectedView: Int
-	
-	public init(selectedView: Int) {
-		UITabBar.appearance().barTintColor = UIColor(ColorManager.appPrimary)
-		_selectedView = State(initialValue: selectedView)
-	}
-	
-	public var body: some View {
-		TabView(selection: $selectedView, content: {
-			NavigationView {
+
+    private let sdk = MealsSDK()
+    private let locationManager = LocationManager()
+
+    @State var selectedView: Int
+
+    public init(selectedView: Int) {
+        UITabBar.appearance().barTintColor = UIColor(ColorManager.appPrimary)
+        _selectedView = State(initialValue: selectedView)
+    }
+
+    public var body: some View {
+        TabView(selection: $selectedView, content: {
+            NavigationView {
                 MealMapView(viewModel: .init(sdk: sdk, locationManager: locationManager))
-			}
-			.navigationBarHidden(true)
-			.tabItem {
-				Image(systemName: Strings.TabView.Images.find)
-				Text(Strings.TabView.findButtonText)
-			}.tag(0)
-			.accentColor(ColorManager.appPrimary)
-			
-			NavigationView {
-				//TODO: Change this to the settings screen, once it is implemented.
-				AddMealView(sdk: sdk, locationManager: locationManager)
-			}
-			.navigationBarHidden(true)
-			.tabItem {
-				Image(systemName: Strings.TabView.Images.settings)
-				Text(Strings.TabView.settings)
-			}.tag(1)
-			.accentColor(ColorManager.appPrimary)
-		})
-		.accentColor(.white)
-	}
+            }
+            .navigationBarHidden(true)
+            .tabItem {
+                Image(systemName: Strings.TabView.Images.find)
+                Text(Strings.TabView.findButtonText)
+            }.tag(0)
+            .accentColor(ColorManager.appPrimary)
+
+            NavigationView {
+                //TODO: Change this to the settings screen, once it is implemented.
+                AddMealView(sdk: sdk, locationManager: locationManager)
+            }
+            .navigationBarHidden(true)
+            .tabItem {
+                Image(systemName: Strings.TabView.Images.settings)
+                Text(Strings.TabView.settings)
+            }.tag(1)
+            .accentColor(ColorManager.appPrimary)
+        })
+        .accentColor(.white)
+    }
 }
 
 struct TabAppView_Previews: PreviewProvider {
-	static var previews: some View {
-		TabAppView(selectedView: 1)
-	}
+    static var previews: some View {
+        TabAppView(selectedView: 1)
+    }
 }
