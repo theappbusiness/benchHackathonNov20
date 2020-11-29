ca65df3eed8293e6bb342a1eb129879151f894be
Dan Smtih <dan.smith@kinandcarta.com>
2020-11-29 12:08:19 +0000

    Adds MapViewModel, permissions and loads list

--- androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/view/MapsFragment.kt
+++ androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/view/MapsFragment.kt
@@ -1,16 +1,33 @@
 package com.kcc.kmmhackathon.androidHackathonApp.view
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.kcc.kmmhackathon.androidHackathonApp.R
+import com.kcc.kmmhackathon.androidHackathonApp.viewmodel.MapsViewModel
+import com.kinandcarta.feature.find.meal.extension.requestFineLocationPermission
+import com.kinandcarta.feature.find.meal.extension.showToast
+import com.kinandcarta.feature.find.meal.utility.PermissionResultParser
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MapsFragment : Fragment() {
+
+    private val viewModel: MapsViewModel by viewModels()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requestFineLocationPermission(LOCATION_PERMISSION_REQUEST_CODE)
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -36,8 +53,53 @@ class MapsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+        viewModel.state.observe(viewLifecycleOwner, ::onStateChanged)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        val permissionResultParser = PermissionResultParser()
+        if (permissionResultParser.isFineLocationPermissionsGranted(permissions, grantResults)) {
+            viewModel.startUpdatingLocation()
+        } else {
+            showToast("Location permissions are required to find meals")
+        }
+    }
+
+    private fun setupUI() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun onStateChanged(state: MapsViewModel.State) {
+        when (state) {
+            MapsViewModel.State.LoadingMeals ->
+                Log.i("Map view", "Loading meals")
+            is MapsViewModel.State.LoadedMeals ->
+                onLoadedMeals(state)
+            is MapsViewModel.State.Failed ->
+                onFailure(state.failure)
+        }
+    }
+
+    private fun onLoadedMeals(state: MapsViewModel.State.LoadedMeals) {
+        Log.i("Maps", "Meals to display ${state.meals}") // TODO: Display markers
+    }
+
+    private fun onFailure(failure: MapsViewModel.Failure) {
+        when (failure) {
+            is MapsViewModel.Failure.LoadingMealsFailed -> {
+                showToast(failure.localizedMessage ?: "An unexpected error occurred loading meals")
+            }
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
---
+++ androidHackathonApp/src/main/java/com/kcc/kmmhackathon/androidHackathonApp/viewmodel/MapsViewModel.kt
@@ -0,0 +1,88 @@
+package com.kcc.kmmhackathon.androidHackathonApp.viewmodel
+
+import android.location.Location
+import androidx.annotation.RequiresPermission
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.kcc.kmmhackathon.shared.MealsSDK
+import com.kcc.kmmhackathon.shared.entity.Meal
+import com.kcc.kmmhackathon.shared.utility.DistanceUnit
+import com.kinandcarta.feature.find.meal.viewmodel.FindMealViewModel
+import kotlinx.coroutines.launch
+
+typealias Meals = List<Meal>
+
+class MapsViewModel @ViewModelInject constructor(
+    private val fusedLocationProviderClient: FusedLocationProviderClient
+) : ViewModel() {
+
+    sealed class State {
+        object LoadingMeals : State()
+        data class LoadedMeals(val meals: Meals, val distanceUnit: DistanceUnit) : State()
+        data class Failed(val failure: Failure) : State()
+    }
+
+    sealed class Failure(cause: Throwable) : Throwable(cause) {
+        class LoadingMealsFailed(cause: Throwable) : Failure(cause)
+    }
+
+    val state: LiveData<State> get() = _state
+
+    private val _state: MutableLiveData<State> = MutableLiveData(State.LoadingMeals)
+    private var lastLocation: Location? = null
+    private val sdk = MealsSDK()
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            lastLocation = locationResult?.lastLocation
+            updateMeals()
+        }
+    }
+
+    override fun onCleared() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    @RequiresPermission("android.permission.ACCESS_FINE_LOCATION")
+    fun startUpdatingLocation() {
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            lastLocation = it
+            updateMeals()
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            createLocationRequest(),
+            locationCallback,
+            null
+        )
+    }
+
+    private fun createLocationRequest(): LocationRequest {
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 100000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        return locationRequest
+    }
+
+    fun updateMeals() {
+        val location = lastLocation ?: return
+        val distanceUnit = DistanceUnit.miles
+        _state.value = State.LoadingMeals
+        viewModelScope.launch {
+            kotlin.runCatching {
+                sdk.getSortedMeals(location.latitude, location.longitude, distanceUnit)
+            }.onSuccess {
+                _state.value = State.LoadedMeals(it, distanceUnit)
+            }.onFailure {
+                _state.value = State.Failed(Failure.LoadingMealsFailed(it))
+            }
+        }
+    }
+
+}
