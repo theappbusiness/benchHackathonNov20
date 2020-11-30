f6f7667ec29de4106161ff1bcbe38e5caa07009d
Dan Smtih <dan.smith@kinandcarta.com>
2020-11-30 15:16:50 +0000

    Adds human readable intervals to locationRequest

--- feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/viewmodel/FindMealViewModel.kt
+++ feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/viewmodel/FindMealViewModel.kt
@@ -80,9 +80,11 @@ class FindMealViewModel @ViewModelInject constructor(
     }
 
     private fun createLocationRequest(): LocationRequest {
+        val oneMinuteFortySecondsInMs: Long = 100000
+        val fiveSecondsInMs: Long = 5000
         val locationRequest = LocationRequest()
-        locationRequest.interval = 100000
-        locationRequest.fastestInterval = 5000
+        locationRequest.interval = oneMinuteFortySecondsInMs
+        locationRequest.fastestInterval = fiveSecondsInMs
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         return locationRequest
     }
