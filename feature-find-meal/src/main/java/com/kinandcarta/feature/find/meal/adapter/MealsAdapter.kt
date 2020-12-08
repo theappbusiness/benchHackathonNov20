1ad4f6699d36a288eb70010d0612c452d5c6988c
Dan Smtih <dan.smith@kinandcarta.com>
2020-12-08 15:10:53 +0000

    Use bindings in MealsAdapter and mini refactor

--- feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/adapter/MealsAdapter.kt
+++ feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/adapter/MealsAdapter.kt
@@ -1,10 +1,7 @@
 package com.kinandcarta.feature.find.meal.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.ListAdapter
@@ -12,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kcc.kmmhackathon.shared.entity.Meal
 import com.kcc.kmmhackathon.shared.utility.DistanceUnit
 import com.kinandcarta.feature.find.meal.R
+import com.kinandcarta.feature.find.meal.databinding.MealItemRowBinding
 import com.kinandcarta.feature.find.meal.extension.getPortionsString
 
 class MealsAdapter(
@@ -19,62 +17,59 @@ class MealsAdapter(
     private val clickListener: (String, Int) -> Unit
 ) : ListAdapter<Meal, MealsAdapter.MealsViewHolder>(MealDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        LayoutInflater.from(parent.context)
-            .inflate(R.layout.meal_item_row, parent, false)
-            .run(::MealsViewHolder)
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MealsViewHolder {
+       val itemBinding = MealItemRowBinding.inflate(
+           LayoutInflater.from(parent.context), parent, false)
+        return MealsViewHolder(itemBinding)
+    }
 
     override fun onBindViewHolder(holder: MealsViewHolder, position: Int) {
         val item = getItem(position)
         holder.bindData(item, position)
     }
 
-    inner class MealsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nameView = itemView.findViewById<TextView>(R.id.mealName)
-        private val tempView = itemView.findViewById<TextView>(R.id.mealTemp)
-        private val infoView = itemView.findViewById<TextView>(R.id.mealInfo)
-        private val distanceView = itemView.findViewById<TextView>(R.id.mealDistance)
-        private val availableView = itemView.findViewById<TextView>(R.id.mealAvailable)
-        private val expiryView = itemView.findViewById<TextView>(R.id.mealExpiry)
-        private val portionsView = itemView.findViewById<TextView>(R.id.mealPortions)
-        private val reserveButton = itemView.findViewById<Button>(R.id.reserveButton)
+    inner class MealsViewHolder(private val itemBinding: MealItemRowBinding): RecyclerView.ViewHolder(itemBinding.root) {
 
         fun bindData(meal: Meal, position: Int) {
-            nameView.text = meal.name
-            val tempString = if (meal.hot) "Hot" else "Cold"
-            val tempColor = if (meal.hot) R.color.colorHot else R.color.colorCold
-            tempView.text = tempString
-            tempView.setTextColor(ContextCompat.getColor(itemView.context, tempColor))
+            itemBinding.mealName.text = meal.name
+            itemBinding.mealTemp.text = if (meal.hot) "Hot" else "Cold"
+            setupMealTemp(meal.hot)
+            setupMealInfo(meal.info)
+            itemBinding.mealDistance.text = "${meal.distance} ${distanceUnit}"
+            itemBinding.mealAvailable.text = "Available: ${meal.availableFromDate}"
+            itemBinding.mealExpiry.text = "Expires: ${meal.expiryDate}"
+            itemBinding.mealPortions.text = "#    ${meal.quantity.getPortionsString()}"
+            setupReserveButton(meal.quantity, meal.id, position)
+        }
 
-            if (meal.info.isNullOrEmpty()) {
-                infoView.isVisible = false
+        private fun setupMealTemp(isHot: Boolean) {
+            val tempString = if (isHot) "Hot" else "Cold"
+            val tempColor = if (isHot) R.color.colorHot else R.color.colorCold
+            itemBinding.mealTemp.text = tempString
+            itemBinding.mealTemp.setTextColor(ContextCompat.getColor(itemBinding.root.context, tempColor))
+        }
+
+        private fun setupMealInfo(info: String?) {
+            if (info.isNullOrEmpty()) {
+                itemBinding.mealInfo.isVisible = false
             } else {
-                infoView.text = "Info: ${meal.info}"
+                itemBinding.mealInfo.text = "Info: ${info}"
             }
+        }
 
-            distanceView.text = "${meal.distance} ${distanceUnit}"
 
-            availableView.text = "Available: ${meal.availableFromDate}"
-            expiryView.text = "Expires: ${meal.expiryDate}"
+        private fun setupReserveButton(quantity: Int, mealId: String, position: Int) {
+            val hasPortions = quantity > 0
+            val reserveButton = itemBinding.reserveButton
 
-            portionsView.text = "#    ${meal.quantity.getPortionsString()}"
+            reserveButton.text = if (hasPortions) "Reserve a portion" else "Unavailable"
 
-            val hasPortions = meal.quantity > 0
-            val reserveButtonText = if (hasPortions) "Reserve a portion" else "Unavailable"
-            val reserveButtonColor =
-                if (hasPortions) R.color.colorReserve else R.color.colorUnavailable
-            reserveButton.text = reserveButtonText
-            reserveButton.setBackgroundColor(
-                ContextCompat.getColor(
-                    itemView.context,
-                    reserveButtonColor
-                )
-            )
+            val reserveButtonColor = if (hasPortions) R.color.colorReserve else R.color.colorUnavailable
+            reserveButton.setBackgroundColor(ContextCompat.getColor(itemBinding.root.context, reserveButtonColor))
 
             if (hasPortions) {
                 reserveButton.setOnClickListener {
-                    clickListener(meal.id, position)
+                    clickListener(mealId, position)
                 }
             } else {
                 reserveButton.setOnClickListener(null)
@@ -82,3 +77,4 @@ class MealsAdapter(
         }
     }
 }
+
