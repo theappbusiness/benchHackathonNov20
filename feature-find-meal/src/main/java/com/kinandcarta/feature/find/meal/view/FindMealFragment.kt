169bd804c9cffb59c969f7d2684f72ca48c9d5a6
Dan Smtih <dan.smith@kinandcarta.com>
2020-12-09 10:46:44 +0000

    Remove leftover log

--- feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/view/FindMealFragment.kt
+++ feature-find-meal/src/main/java/com/kinandcarta/feature/find/meal/view/FindMealFragment.kt
@@ -1,7 +1,6 @@
 package com.kinandcarta.feature.find.meal.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -69,7 +68,6 @@ class FindMealFragment : Fragment() {
     private fun onLoadedMeals(state: DisplayMealsViewModel.State.LoadedMeals) {
         progressBarView.isVisible = false
         mealsAdapter.submitList(state.meals)
-        Log.i("Find meal fragment", "onLoadedMeals ${state.meals}")
     }
 
     private fun onReservedMeal(state: DisplayMealsViewModel.State.ReservedMeal) {
