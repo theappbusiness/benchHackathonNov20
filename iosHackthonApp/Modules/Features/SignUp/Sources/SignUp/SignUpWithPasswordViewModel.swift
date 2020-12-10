820217aba7fe4f18cd806983f72a7157cc41dcb9
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-12-10 12:06:16 +0000

    removed api key from ios side

--- iosHackthonApp/Modules/Features/SignUp/Sources/SignUp/SignUpWithPasswordViewModel.swift
+++ iosHackthonApp/Modules/Features/SignUp/Sources/SignUp/SignUpWithPasswordViewModel.swift
@@ -12,7 +12,7 @@ import Components
 import Strings
 
 public final class SignUpWithPasswordViewModel: ObservableObject {
-
+  
   var email: String
   @Published var password: String = ""
   @Published var moveToNextScreen: Bool = false
@@ -26,16 +26,14 @@ public final class SignUpWithPasswordViewModel: ObservableObject {
   var signUpWithPasswordButtonTitle: String = Strings.SignUp.signUpWithPasswordButtonTitle
   var signUpWithPasswordFailedTitle: String = Strings.SignUp.signUpWithPasswordFailed
   var signUpWithPasswordFailedInfo: String = Strings.SignUp.signUpWithPasswordFailedInfo
-
+  
   public init(email: String, firebase: FirebaseAuthenticationStore, authorizationStore: AuthorizationStore) {
     self.email = email
     self.firebase = firebase
     self.authorizationStore = authorizationStore
   }
-
+  
   func signUp(email: String, password: String) {
-    //TODO: API key to be handled in shared layer. remove the apikey parameter once implemented
-    // To run the project for now, add the api key and run
     isLoading = true
     firebase.signUp(email: email, password: password, returnSecureToken: true, completionHandler: { result, _ in
       if result?.idToken != nil {
