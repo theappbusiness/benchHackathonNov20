42d82c94afb37eed5cc6f6c8aca5701ff7a1eef5
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-12-11 10:27:16 +0000

    update lat long on view appear

--- iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealView.swift
+++ iosHackthonApp/Modules/Features/AddMeal/Sources/AddMeal/AddMealView.swift
@@ -73,6 +73,9 @@ public struct AddMealView: View {
         .navigationBarTitle(Strings.AddMealScreen.addMeal)
         .onAppear {
           address = "\(locationManager.address)"
+          latitude = Float(locationManager.userLatitude)
+          longitude = Float(locationManager.userLongitude)
+
         }
       }
       .alert(isPresented: $showingAlert) {
